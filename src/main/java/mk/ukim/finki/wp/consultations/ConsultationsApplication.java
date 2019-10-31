1bf56b7b1d188c0e7ab5630d04b789562ca57f89
Tea Zdravkovska <tea.zdravkovska@hotmail.com>
2019-10-31 23:18:19 +0100

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wp/consultations/ConsultationsApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.consultations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/wp/consultations/ConsultationsApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.consultations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
