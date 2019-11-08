33e3c4423cc6642b662a0794e96cfd61843ef25c
Tea Zdravkovska <tea.zdravkovska@hotmail.com>
2019-11-08 13:23:13 +0100

    Initial commit

--- src/main/java/mk/ukim/finki/wp/consultations/ConsultationsApplication.java
+++ src/main/java/mk/ukim/finki/wp/consultations/ConsultationsApplication.java
@@ -2,8 +2,10 @@ package mk.ukim.finki.wp.consultations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ConsultationsApplication {
 
     public static void main(String[] args) {
