86e413cbeaf45d8eedaa6522778e227c01c4519e
Tea Zdravkovska <tea.zdravkovska@hotmail.com>
2019-12-02 23:32:35 +0100

    exceptions

---
+++ src/main/java/mk/ukim/finki/wp/consultations/exceptions/InvalidIngredientException.java
@@ -0,0 +1,5 @@
+package mk.ukim.finki.wp.consultations.exceptions;
+
+
+public class InvalidIngredientException extends RuntimeException {
+}
---
+++ src/main/java/mk/ukim/finki/wp/consultations/exceptions/InvalidPizzaException.java
@@ -0,0 +1,4 @@
+package mk.ukim.finki.wp.consultations.exceptions;
+
+public class InvalidPizzaException extends RuntimeException {
+}
