d2960ecc2670e2f9ac6d199ae3ed757a7f401720
Tea Zdravkovska <tea.zdravkovska@hotmail.com>
2019-12-02 22:28:41 +0100

    qualifier added into the pizza service implementation in order for the service to know which implementation to use

--- src/main/java/mk/ukim/finki/wp/consultations/service/impl/PizzaServiceImpl.java
+++ src/main/java/mk/ukim/finki/wp/consultations/service/impl/PizzaServiceImpl.java
@@ -3,6 +3,7 @@ package mk.ukim.finki.wp.consultations.service.impl;
 import mk.ukim.finki.wp.consultations.model.Pizza;
 import mk.ukim.finki.wp.consultations.repository.PizzaRepository;
 import mk.ukim.finki.wp.consultations.service.PizzaService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +12,11 @@ import java.util.List;
 public class PizzaServiceImpl implements PizzaService {
     private final PizzaRepository pizzaRepository;
 
-    public PizzaServiceImpl(PizzaRepository pizzaRepository) {
+    // We must add a qualifier in order the service to know which implementation of the pizzaRepository to use
+    // Remember,there are two implementations of the PizzaRepository
+    // We'll use the jpa pizza implementation
+
+    public PizzaServiceImpl(@Qualifier("jpaPizzaRepositoryImpl") PizzaRepository pizzaRepository) {
         this.pizzaRepository = pizzaRepository;
     }
 
