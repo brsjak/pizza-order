5ae04ffe9c8a7609f7757a062d7c05c2103ad093
Tea Zdravkovska <tea.zdravkovska@hotmail.com>
2019-12-04 00:21:39 +0100

    modified data holder file

--- src/main/java/mk/ukim/finki/wp/consultations/bootstrap/DataHolder.java
+++ src/main/java/mk/ukim/finki/wp/consultations/bootstrap/DataHolder.java
@@ -28,45 +28,100 @@ public class DataHolder {
 
     @PostConstruct
     public void init(){
-        ingredients.add(new Ingredient("tomato sauce",true,100,true));
-        ingredients.add(new Ingredient("fresh cream",true,500,false));
-        ingredients.add(new Ingredient("mozzarella",false,500,false));
-        ingredients.add(new Ingredient("bacon",false,2000,false));
-        ingredients.add(new Ingredient("dough",false,5000,true));
-        ingredients.add(new Ingredient("mushrooms",true,4000,true));
-        ingredients.add(new Ingredient("ricotta",false,5000,false));
-        ingredients.add(new Ingredient("pizza sauce",true,5000,true));
-        ingredients.add(new Ingredient("olive oil",false,6000,true));
-        ingredients.add(new Ingredient("pepperoni",true,4000,true));
-        ingredients.add(new Ingredient("cheddar",true,3000,false));
-        ingredients.add(new Ingredient("cheese",false,3000,false));
-        ingredients.add(new Ingredient("ham",true,3000,false));
-        ingredients.add(new Ingredient("onions",true,3000,true));
-        ingredients.add(new Ingredient("barbecue sauce",true,3000,false));
-        ingredients.add(new Ingredient("sausage",false,3000,false));
-        ingredients.add(new Ingredient("chicken meat",false,3000,false));
-        ingredients.add(new Ingredient("taleggio",false,3000,false));
-        ingredients.add(new Ingredient("cascarpone",true,3000,false));
-        ingredients.add(new Ingredient("gorgonzola",true,3000,false));
-        ingredients.add(new Ingredient("parmesan",true,3000,false));
-        ingredients.add(new Ingredient("beef",true,6000,false));
-        ingredients.add(new Ingredient("mascarpone",true,6000,false));
+        Ingredient tomato_sauce = new Ingredient("tomato sauce",true,100,true,new ArrayList<>());
+        Ingredient fresh_cream = new Ingredient("fresh cream",true,500,false,new ArrayList<>());
+        Ingredient mozzarella = new Ingredient("mozzarella",false,500,false,new ArrayList<>());
+        Ingredient bacon = new Ingredient("bacon",true,100,true,new ArrayList<>());
+        Ingredient dough = new Ingredient("dough",true,100,true,new ArrayList<>());
+        Ingredient mushrooms = new Ingredient("mushrooms",true,100,true,new ArrayList<>());
+        Ingredient ricotta = new Ingredient("ricotta",true,100,true,new ArrayList<>());
+        Ingredient pizza_sauce = new Ingredient("pizza sauce",true,100,true,new ArrayList<>());
+        Ingredient olive_oil = new Ingredient("olive oli",true,100,true,new ArrayList<>());
+        Ingredient pepperoni = new Ingredient("pepperoni",true,100,true,new ArrayList<>());
+        Ingredient cheddar = new Ingredient("cheddar",true,100,true,new ArrayList<>());
+        Ingredient cheese = new Ingredient("cheese",true,100,true,new ArrayList<>());
+        Ingredient ham = new Ingredient("ham",true,100,true,new ArrayList<>());
+        Ingredient onions = new Ingredient("onions",true,100,true,new ArrayList<>());
+        Ingredient barbecue_sauce = new Ingredient("barbecue sauce",true,100,true,new ArrayList<>());
+        Ingredient chicken_meat = new Ingredient("chicken meat",true,100,true,new ArrayList<>());
+        Ingredient taleggio = new Ingredient("tomato taleggio",true,100,true,new ArrayList<>());
+        Ingredient sausage = new Ingredient("sausage",true,100,true,new ArrayList<>());
+        Ingredient gorgonzola = new Ingredient("gorgonzola",true,100,true,new ArrayList<>());
+        Ingredient parmesan = new Ingredient("parmesan",true,100,true,new ArrayList<>());
+        Ingredient beef = new Ingredient("beef",true,100,true,new ArrayList<>());
+        Ingredient mascarpone = new Ingredient("mascarpone",true,100,true,new ArrayList<>());
+
+        ingredients.add(tomato_sauce);
+        ingredients.add(fresh_cream);
+        ingredients.add(mozzarella);
+        ingredients.add(bacon);
+        ingredients.add(dough);
+        ingredients.add(mushrooms);
+        ingredients.add(ricotta);
+        ingredients.add(pizza_sauce);
+        ingredients.add(olive_oil);
+        ingredients.add(pepperoni);
+        ingredients.add(cheese);
+        ingredients.add(cheddar);
+        ingredients.add(ham);
+        ingredients.add(onions);
+        ingredients.add(barbecue_sauce);
+        ingredients.add(chicken_meat);
+        ingredients.add(taleggio);
+        ingredients.add(sausage);
+        ingredients.add(gorgonzola);
+        ingredients.add(parmesan);
+        ingredients.add(beef);
+        ingredients.add(mascarpone);
+
+        pizzas.add(new Pizza("Margharita",new ArrayList<>(),true));
+        pizzas.add(new Pizza("Carbonara",new ArrayList<>(),false));
+        pizzas.add(new Pizza("Vegetariana",new ArrayList<>(),true));
+        pizzas.add(new Pizza("Calzone",new ArrayList<>(),false));
+        pizzas.add(new Pizza("Cheddar",new ArrayList<>(),false));
+        pizzas.add(new Pizza("Capricciosa",new ArrayList<>(),false));
+        pizzas.add(new Pizza("Burger Classic",new ArrayList<>(),false));
+        pizzas.add(new Pizza("Burger Barbecue",new ArrayList<>(),false));
+        pizzas.add(new Pizza("Pepperoni",new ArrayList<>(),true));
+        pizzas.add(new Pizza("Quattro Formaggi",new ArrayList<>(),false));
+
+        pizzas.get(0).addIngredients(tomato_sauce);
+        pizzas.get(0).addIngredients(mozzarella);
+        pizzas.get(1).addIngredients(fresh_cream);
+        pizzas.get(1).addIngredients(mozzarella);
+        pizzas.get(1).addIngredients(bacon);
+        pizzas.get(2).addIngredients(tomato_sauce);
+        pizzas.get(2).addIngredients(mushrooms);
+        pizzas.get(3).addIngredients(dough);
+        pizzas.get(3).addIngredients(ricotta);
+        pizzas.get(3).addIngredients(pepperoni);
+        pizzas.get(3).addIngredients(pizza_sauce);
+        pizzas.get(3).addIngredients(olive_oil);
+        pizzas.get(4).addIngredients(cheddar);
+        pizzas.get(4).addIngredients(tomato_sauce);
+        pizzas.get(5).addIngredients(tomato_sauce);
+        pizzas.get(5).addIngredients(cheese);
+        pizzas.get(5).addIngredients(ham);
+        pizzas.get(6).addIngredients(barbecue_sauce);
+        pizzas.get(6).addIngredients(beef);
+        pizzas.get(6).addIngredients(mozzarella);
+        pizzas.get(6).addIngredients(onions);
+        pizzas.get(7).addIngredients(ham);
+        pizzas.get(7).addIngredients(chicken_meat);
+        pizzas.get(7).addIngredients(onions);
+        pizzas.get(8).addIngredients(tomato_sauce);
+        pizzas.get(8).addIngredients(mozzarella);
+        pizzas.get(8).addIngredients(sausage);
+        pizzas.get(9).addIngredients(taleggio);
+        pizzas.get(9).addIngredients(mascarpone);
+        pizzas.get(9).addIngredients(gorgonzola);
+        pizzas.get(9).addIngredients(parmesan);
 
 
-
-        pizzas.add(new Pizza("Margherita",new ArrayList<Ingredient>(Arrays.asList(ingredients.get(0),ingredients.get(2))),true));
-        pizzas.add(new Pizza("Carbonara",new ArrayList<Ingredient>(Arrays.asList(ingredients.get(1),ingredients.get(2),ingredients.get(3))),false));
-        pizzas.add(new Pizza("Vegetariana",new ArrayList<Ingredient>(Arrays.asList(ingredients.get(0),ingredients.get(3))),true));
-        pizzas.add(new Pizza("Calzone",new ArrayList<Ingredient>(Arrays.asList(ingredients.get(4),ingredients.get(6),ingredients.get(9),ingredients.get(7))),false));
-        pizzas.add(new Pizza("Cheddar",new ArrayList<Ingredient>(Arrays.asList(ingredients.get(10),ingredients.get(0))),false));
-        pizzas.add(new Pizza("Capricciosa",new ArrayList<Ingredient>(Arrays.asList(ingredients.get(0), ingredients.get(11),ingredients.get(12))),false));
-        pizzas.add(new Pizza("Burger Classic",new ArrayList<Ingredient>(Arrays.asList(ingredients.get(21),ingredients.get(14),ingredients.get(1),ingredients.get(13))),false));
-        pizzas.add(new Pizza("Burger Barbecue",new ArrayList<Ingredient>(Arrays.asList(ingredients.get(12),ingredients.get(16),ingredients.get(13))),false));
-        pizzas.add(new Pizza("Pepperoni",new ArrayList<Ingredient>(Arrays.asList(ingredients.get(0),ingredients.get(2),ingredients.get(15))),true));
-        pizzas.add(new Pizza("Quattro Formaggi",new ArrayList<Ingredient>(Arrays.asList(ingredients.get(7),ingredients.get(22),ingredients.get(19),ingredients.get(20))),false));
-
-        this.jpaPizzaRepository.saveAll(pizzas);
-        this.jpaIngredientRepository.saveAll(ingredients);
+        if(this.jpaPizzaRepository.count() == 0 && this.jpaIngredientRepository.count() == 0) {
+            this.jpaPizzaRepository.saveAll(pizzas);
+            this.jpaIngredientRepository.saveAll(ingredients);
+        }
 
     }
 
