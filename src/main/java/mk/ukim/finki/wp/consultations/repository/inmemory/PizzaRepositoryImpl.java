186cec4c48ffa2d0be09bee0409dea665fea32a6
Tea Zdravkovska <tea.zdravkovska@hotmail.com>
2019-12-03 14:08:21 +0100

    methods in pizza repository implementation overrided,not implemented hence it's assigned as profile in memory repository

--- src/main/java/mk/ukim/finki/wp/consultations/repository/inmemory/PizzaRepositoryImpl.java
+++ src/main/java/mk/ukim/finki/wp/consultations/repository/inmemory/PizzaRepositoryImpl.java
@@ -1,12 +1,14 @@
 package mk.ukim.finki.wp.consultations.repository.inmemory;
 
 import mk.ukim.finki.wp.consultations.bootstrap.DataHolder;
+import mk.ukim.finki.wp.consultations.model.Ingredient;
 import mk.ukim.finki.wp.consultations.model.Pizza;
 import mk.ukim.finki.wp.consultations.repository.PizzaRepository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Profile("in-memory-repo")
 @Repository
@@ -14,5 +16,30 @@ public class PizzaRepositoryImpl implements PizzaRepository {
     @Override
     public List<Pizza> listPizzas() {
         return DataHolder.pizzas;
+    }
+
+    @Override
+    public Pizza savePizza(Pizza pizza) {
+        return null;
+    }
+
+    @Override
+    public Pizza savePizza(String name, List<Ingredient> ingredients, boolean veggie) {
+        return null;
+    }
+
+    @Override
+    public Optional<Pizza> getPizzaByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deletePizza(String name) {
+
+    }
+
+    @Override
+    public void deletePizza(Pizza pizza) {
+
     }
 }
